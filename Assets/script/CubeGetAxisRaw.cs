e84894c959c9f6800be5cef46fd0b431984047c0
williamfr7 <70350291+williamfr7@users.noreply.github.com>
2021-01-19 09:46:19 -0400

    Initial commit

---
+++ Assets/script/CubeGetAxisRaw.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGetAxisRaw : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        this.transform.Translate(x * speed * Time.deltaTime, 0.0f, y * speed * Time.deltaTime);
+        if (Input.GetKeyDown("space")) {
+
+            this.transform.Translate(Vector3.up);
+        }
+    }
+}
---
+++ Assets/script/PlayerTriggerControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerTriggerControl : MonoBehaviour
+{
+    Vector3 originalPosition;
+    public GameObject CheckpointText;
+
+    private void Start()
+    {
+        CheckpointText.SetActive(false);
+        originalPosition = gameObject.transform.position;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "CheckPoint")
+        {
+            originalPosition = gameObject.transform.position;
+            CheckpointText.SetActive(true);
+        }
+
+        if (other.tag == "KillBox")
+        {
+            gameObject.transform.position = originalPosition;
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "CheckPoint") {
+
+            CheckpointText.SetActive(false);
+        }
+    }
+}
